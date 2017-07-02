b25d6881954988766e72bbdbffeabb17c3ebaf20
Kamyl <kamillcybulski@gmail.com>
2017-07-02 18:49:24 +0200

    Initial commit. Setup.

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+class App extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+    
+  }
+
+  render() {
+    return (
+      <div className="iii">Weather yo!</div>
+    );
+  }
+}
+
+export default App;
